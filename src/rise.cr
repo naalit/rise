7697abb397f98531c3a7bf58e26c1206765f0c24
Sam <sam@dirkback.com>
2017-04-28 17:01:12 -0500

    Can specify website from command line now, and Rise will fetch it and parse it.

--- src/rise.cr
+++ src/rise.cr
@@ -1,17 +1,24 @@
 require "./rise/*"
+require "http/client"
 
 module Rise
 
 engine = Engine.new
-puts engine.run(%{
-<html>
-    <body>
-        <h1>Title</h1>
-        <div id="main" class="test">
-            <p>Hello <em>world</em>!</p>
-        </div>
-    </body>
-</html>
-})
+if !ARGV[0]
+  puts engine.run(%{
+  <html>
+      <body>
+          <h1>Title</h1>
+          <div id="main" class="test">
+              <p>Hello <em>world</em>!</p>
+            </div>
+        </body>
+    </html>
+  })
+else
+  response = HTTP::Client.get ARGV[0]
+  puts response.body
+  puts engine.run(response.body)
+end
 
 end # Those cascading end's look cool
